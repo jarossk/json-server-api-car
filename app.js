20db6cb5978fbc0a88c56e06a456518d5f1a60a8
Jarek <klata.jarek@gmail.com>
2018-10-16 13:45:17 +0100

    config app

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
 const path = require('path');
+const jsonServer = require('json-server');
+//const express = require('express');
+const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
-// const router = jsonServer.router('db.json');
+//const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+
+console.log(middlewares[1]);
 
 server.use(middlewares);
 
-server.get('/', (req, res) => {
+server.get('/vehicles', (req, res) => {
     res.jsonp(req.query);
 });
 
+//server.use(middlewares.serveStatic('public'))
+//server.use('/', express.static(path.join(__dirname, 'static')));
+
+//server.get('/', (req, res) => res.sendFile('index.html'));
+
+
+
 server.use(router);
 server.listen(3000, () => {
-    console.log('JSON server is runnung');
+    console.log('JSON server app listening on port 3000!');
 });
 
 /*
-const express = require('express')
-const app = express()
+
 const path = require('path');
 
-// app.use(express.static('public'))
-//app.use('/', express.static(path.join(__dirname, 'static')))
 
-app.get('/', (req, res) => res.sendFile('index.html'))
 
 // app.get('/', (req, res) => res.send('Hello World!!!'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
